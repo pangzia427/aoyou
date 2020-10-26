8961fd92f4664f9b5d73bc6c1c7459a3fed9ff8d
panchao <908853398@qq.com>
2020-10-26 20:06:39 +0800

    新增了controller

---
+++ src/main/java/com/example/aoyou/AoyouApplication.java
@@ -0,0 +1,13 @@
+package com.example.aoyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AoyouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AoyouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aoyou/AoyouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aoyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AoyouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
